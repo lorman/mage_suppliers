52706aa42f3eb8c12977820a3c7bd5df90b0104e
Eugene Lorman <eugene.lorman@gmail.com>
2016-09-19 10:53:22 -0500

    Added toOptionList

--- app/code/local/HubCo/Suppliers/Model/Supplier.php
+++ app/code/local/HubCo/Suppliers/Model/Supplier.php
@@ -8,5 +8,31 @@ class HubCo_Suppliers_Model_Supplier extends Mage_Core_Model_Abstract
 
     }
 
+    public function toOptionList($multi = false)
+    {
+      $suppliers = array();
+      $allSuppliersCollection = Mage::getModel('suppliers/supplier')
+      ->getCollection();
+      $allSuppliers = $allSuppliersCollection->load()->toArray();
+      foreach ($allSuppliers['items'] as $supplier)
+      {
+        if (!isset($supplier['name'])) {
+          continue;
+        }
+        if ($multi)
+        {
+          $suppliers[$supplier['supplier_id']] = array(
+              'value' => $supplier['supplier_id'],
+              'label' => $supplier['name']
+          );
+        }
+        else
+        {
+          $suppliers[$supplier['supplier_id']] = $supplier['name'];
+        }
+      }
+
+      return $suppliers;
+    }
+
 }
-	 
